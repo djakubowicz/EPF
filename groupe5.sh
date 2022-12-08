ac99825b9ca104a4cdff38875b47870c92ae99a5
djakubowicz <45103701+djakubowicz@users.noreply.github.com>
2022-12-08 09:09:07 +0100

    Update groupe5.sh

--- groupe5.sh
+++ groupe5.sh
@@ -3,7 +3,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 sudo apt update -y
-sudo apt install docker-ce docker-ce-cli containerd.io -y
+sudo apt install docker-ce docker-ce-cli containerd.io docker-compose -y
 sudo mkdir -p /etc/apt/keyrings
 sudo apt-get install docker-compose-plugin -y
 rm -r cultofthecat
