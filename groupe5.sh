eb3a9a4a7059f3454fe5468faa51c9b1f1be9190
djakubowicz <45103701+djakubowicz@users.noreply.github.com>
2022-12-08 09:47:54 +0100

    Update groupe5.sh
    change to heimdall container app

--- groupe5.sh
+++ groupe5.sh
@@ -3,11 +3,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 sudo apt update -y
-sudo apt install docker-ce docker-ce-cli containerd.io docker-compose -y
-sudo mkdir -p /etc/apt/keyrings
-sudo apt-get install docker-compose-plugin -y
-rm -r cultofthecat
-git clone https://github.com/epfmng/cultofthecat.git
-cd cultofthecat
-docker compose down -v
-docker compose up db app -d
+sudo apt install docker-ce docker-ce-cli containerd.io -y
+sudo docker volume create heimdall
+sudo docker run --name=heimdall -e PUID=1000 -e PGID=1000 -e TZ=america/new_york -p 8006:80 -p 406:443 -v heimdall:/config --restart unless-stopped linuxserver/heimdall
